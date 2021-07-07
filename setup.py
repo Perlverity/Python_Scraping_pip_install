cdf98e9c33dfe280dcf38b00c74e624bd15e6d99
Perlverity <snowman.ss.6124@gmail.com>
2021-07-07 10:04:58 +0900

    CLI

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ from setuptools import setup
 
 requires = ["requests>=2.14.2"]
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='Scraping_Perlverity_Python',
     version='1.0.0',
-    description='# pip install Success!! pip install でライブラリインストールでWebスクレイピング起動可能。',
+    description='pip install Success!! pip install でライブラリインストールでWebスクレイピング起動可能。',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/whatever/whatever',
     author='Perlverity',
     author_email='your@address.com',
