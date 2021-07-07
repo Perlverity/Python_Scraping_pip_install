f0dc97ae9e17b02dc65aec030fac879cee8524ff
Perlverity <snowman.ss.6124@gmail.com>
2021-07-07 09:33:32 +0900

    pypi upload

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+
+requires = ["requests>=2.14.2"]
+
 
 setup(
     name='Scraping',
     version='1.0.0',
-    packages=find_packages()
+    description='Awesome library',
+    url='https://github.com/whatever/whatever',
+    author='Perlverity',
+    author_email='your@address.com',
+    license='MIT',
+    keywords='sample setuptools development',
+    packages=[
+        "Scraping"
+    ],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
 )
