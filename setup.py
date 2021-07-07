adc7e6e4033ac10d475e9126528bf7b8ee8abade
Perlverity <snowman.ss.6124@gmail.com>
2021-07-07 10:01:05 +0900

    CLI

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = ["requests>=2.14.2"]
 setup(
     name='Scraping_Perlverity_Python',
     version='1.0.0',
-    description='Awesome library',
+    description='# pip install Success!! pip install でライブラリインストールでWebスクレイピング起動可能。',
     url='https://github.com/whatever/whatever',
     author='Perlverity',
     author_email='your@address.com',
